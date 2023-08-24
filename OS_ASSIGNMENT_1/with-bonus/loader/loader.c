a73413daf82dc59a677d03fe3cf4b7a706d8ba27
Debjit-Banerji <101322330+Debjit-Banerji@users.noreply.github.com>
2023-08-24 19:24:02 +0530

    Update loader.c

--- OS_ASSIGNMENT_1/with-bonus/loader/loader.c
+++ OS_ASSIGNMENT_1/with-bonus/loader/loader.c
@@ -4,6 +4,7 @@ Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
 int fd;
 
+//Function to check the magic number of the elf file
 int elf_check_file(Elf32_Ehdr *hdr) {
 	if(!hdr) return 0;
 	if(hdr->e_ident[EI_MAG0] != ELFMAG0) {
@@ -25,6 +26,35 @@ int elf_check_file(Elf32_Ehdr *hdr) {
 	return 1;
 }
 
+//Function to check the validity of the elf file
+int elf_check_supported(Elf32_Ehdr *hdr) {
+	if(elf_check_file(hdr) == 0) {
+		printf("Invalid ELF File.\n");
+		return 0;
+	}
+	if(hdr->e_ident[EI_CLASS] != ELFCLASS32) {
+		printf("Unsupported ELF File Class.\n");
+		return 0;
+	}
+	if(hdr->e_ident[EI_DATA] != ELFDATA2LSB) {
+		printf("Unsupported ELF File byte order.\n");
+		return 0;
+	}
+	if(hdr->e_machine != EM_386) {
+		printf("Unsupported ELF File target.\n");
+		return 0;
+	}
+	if(hdr->e_ident[EI_VERSION] != EV_CURRENT) {
+		printf("Unsupported ELF File version.\n");
+		return 0;
+	}
+	if(hdr->e_type != ET_REL && hdr->e_type != ET_EXEC) {
+		printf("Unsupported ELF File type.\n");
+		return 0;
+	}
+	return 1;
+}
+
 /*
  * release memory and other cleanups
  */
@@ -38,22 +68,20 @@ void loader_cleanup() {
 /*
  * Load and run the ELF executable file
  */
-void load_and_run_elf(char** argv) {
-  //used *exe since exe is pointer to argv[1] so to access the argv[1] value we are dereferencing the exe pointer
-  fd = open(argv[1], O_RDONLY);
+void load_and_run_elf(char** exe) {
+  fd = open(exe[1], O_RDONLY);
   size_t offset_vmem;
   void* virtual_mem;
-  
   if(fd != -1){
     //initialising ehdr
     ehdr = (Elf32_Ehdr*)malloc(sizeof(Elf32_Ehdr));
     if(read(fd,ehdr,sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)){
-      printf("Error!");
-      exit(2);
-    }
-    if(elf_check_file(ehdr) == 0){
-      printf("The elf file is not valid!");
+      printf("The elf file header couldn't be read!");
       exit(1);
+    }
+    if(elf_check_supported(ehdr) == 0){
+      printf("The elf file is not valid!");
+      exit(2);
     }
     //initialising phdr
     phdr = (Elf32_Phdr*)malloc(sizeof(Elf32_Phdr));
@@ -62,7 +90,10 @@ void load_and_run_elf(char** argv) {
       //positioning the file pointer to the section from where the program header table starts
       lseek(fd,(ehdr->e_phoff + i*ehdr->e_phentsize),SEEK_SET);
       //reading the segment and making the phdr point to the segment
-      read(fd,phdr,sizeof(Elf32_Phdr));
+      if(read(fd,phdr,sizeof(Elf32_Phdr)) != sizeof(Elf32_Phdr)){
+        printf("The program header couldn't be read!");
+        exit(3);
+      }
       //comparing to see f the phdr p_type is of type "PT_LOAD" (type "PT_LOAD" has value 1)
       if(phdr->p_type == PT_LOAD){
         //checking whether the e_entry lies in the segment corresponding to the prgram header
@@ -71,20 +102,19 @@ void load_and_run_elf(char** argv) {
           virtual_mem = mmap((void*)(phdr->p_vaddr), phdr->p_memsz, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_FIXED, fd, phdr->p_offset);
           //throws error virtual_mem is not allocated properly
           if(virtual_mem == MAP_FAILED){
-            printf("Error in vitual_mem");
+            printf("Error in defining vitual_mem!");
             return;
-            exit(2);
+            exit(4);
           }
-	  //Setting the offset value, from the virtual_mem starting address, where the e_entry is located inside the virtual_mem
-	  offset_vmem =  ehdr->e_entry - phdr->p_vaddr;        
-          }
+          //Setting the offset value, from the virtual_mem starting address, where the e_entry is located inside the virtual_mem
+          offset_vmem = ehdr->e_entry - phdr->p_vaddr;
           break;
         }
       }
     }
   }
   else{
-    printf("Error!!");
+    printf("Error in opening the elf file!");
   }
   //Closing the file
   close(fd);
