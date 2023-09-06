a370e58cfdab4c644f5237202a3d2ac1904f7d45
baljyot25 <132061122+baljyot25@users.noreply.github.com>
2023-09-06 22:49:45 +0530

    Update A2.c

--- OS_Assignment_2/A2.c
+++ OS_Assignment_2/A2.c
@@ -1,51 +1,79 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-#define MAX_INPUT_LENGTH 1024
-char* com[2];
 
-void split(char* command){
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+#define MAX_INPUT_LENGTH 1024
+
+
+char** split(char* command){
     char* s1;
-    com[0] = strtok(command," ");
-    if(com[0] != NULL){
-        if((com[1] = strtok(NULL," ")) == NULL){
-            printf("Error!");
-            exit(2);
+    char* com1;
+    char* args;
+    char** com;
+    com1 = strtok(command," ");
+    if(com1 != NULL){
+        if((args = strtok(NULL," ")) != NULL){
+            while (1)
+            {
+                s1 = strtok(NULL," ");
+                if(s1 == NULL){
+                    break;
+                }
+                strcat(args," ");
+                strcat(args,s1);
+            }
+            com[0] = com1;
+            com[1] = args;
+            com[2] = NULL;
         }
-    }
-    while (1)
-    {
-        s1 = strtok(NULL," ");
-        if(s1 == NULL){
-            break;
+        else{
+            com[0] = com1;
+            com[1] = NULL;
         }
-        strcat(com[1]," ");
-        strcat(com[1],s1);
+        return com;
     }
+    return NULL;
 }
 
-int create_process_and_run(char* command) {
+int create_process_and_run(char** com) {
     int status = fork();
     char* arr[1] = {NULL};
     if(status < 0) {
         printf("Something bad happened\n");
-        return 1;
+        return 0;
     } else if(status == 0) {
-        printf("6\n");
-        printf("%s\n",com[0]);
-        if(strcmp(com[0],"echo")==0){
-            printf("7\n");
-            execv(com[0], *com);
-        }
-        else{
-            printf("8\n");
-            execv(command,arr);
-        }
+        char *args[] = {"/bin/ls", NULL}; // Only specify the path to "ls"
+
+    // Execute the "ls" command using execv
+    // printf("\n");
+    // printf("i am child\n");
+        execv("/bin/ls", args);
+        printf("vhai\n");
+        // printf("6\n");
+        // printf("%s\n",com[0]);
+        // if(strcmp(com[0],"echo")==0){
+        //     printf("7\n");
+        //     execv(com[0], com);
+        // }
+        // else{
+        //     printf("8\n");
+        //     execv(com[0],arr);
+        // }
+        
     }
-    return 0;
+    else{
+        // printf("i am parent\n");
+        wait(&status);
+    }
+    
+    return 1;
+    
 }
 
 void shell_loop() {
@@ -62,13 +90,15 @@ void shell_loop() {
             perror("Error reading input");
             exit(2);
         }
-        split(command);
-        status = create_process_and_run(command);
+        char** com = split(command);
+        status = create_process_and_run(com);
+        
     } while(status);
 }
 
 int main(int argc, char const *argv[])
 {
     shell_loop();
+    printf("int_main\n");
     return 0;
 }
