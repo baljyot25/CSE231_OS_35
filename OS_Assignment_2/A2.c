a658e8d8b8620480aecd33fffd746d747e47b1a5
baljyot25 <132061122+baljyot25@users.noreply.github.com>
2023-09-08 12:49:21 +0530

    handled history

--- OS_Assignment_2/A2.c
+++ OS_Assignment_2/A2.c
@@ -4,151 +4,164 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <time.h>
+#include <semaphore.h>
 
-//Setting the max length any command can have
 #define MAX_INPUT_LENGTH 1024
-//Initialising the file pointer
+
 FILE *f1;
-//Initialising the process ID variable which will store the process IDs of the child processes
+
 int pid;
-//Initiaised the variable to store the start time of execution for each variable
+char line[MAX_INPUT_LENGTH] = "";
+
 char timeofexec[50];
-//Initiaising the variables to store the start and end time of each command executed.
 struct timespec start_time_of_exec;
 struct timespec end_time_of_exec;
 
-//Creating an appropriate array from the given command to be passed to execv function
-char** split(char* command){
-    char* s1;
-    char* com1 = "/bin/";
-    char* args;
-    char** com;
-    //Reading the first 
-    s1 = strtok(command," ");
-    com1 = strcat(com1,s1);
-    if(com1 != NULL){
-        if((args = strtok(NULL," ")) != NULL){
-            while (1)
-            {
-                s1 = strtok(NULL," ");
-                if(s1 == NULL){
-                    break;
-                }
-                strcat(args," ");
-                strcat(args,s1);
-            }
-            com[0] = com1;
-            com[1] = args;
-            com[2] = NULL;
-        }
-        else{
-            com[0] = com1;
-            com[1] = NULL;
-        }
-        return com;
+
+void split(char* command, char** com) {
+    int i = 0;
+    char* token = strtok(command, " ");
+    while (token != NULL) {
+        com[i++] = token;
+        token = strtok(NULL, " ");
     }
-    return NULL;
+    com[i] = NULL; 
 }
 
-void get_time(){
+void get_time() {
     time_t ct;
     ct = time(NULL);
-    strftime(timeofexec,sizeof(timeofexec),"%d-%m-%Y %H:%M:%S",localtime(&ct));
+    strftime(timeofexec, sizeof(timeofexec), "%d-%m-%Y %H:%M:%S", localtime(&ct));
+}
+
+void history() {
+    fputs(line, f1);
+    int c;
+    rewind(f1); 
+    while ((c = fgetc(f1)) != EOF) {
+        putchar(c);
+    }
+    memset(line, '\0', sizeof(line));
 }
 
 int create_process_and_run(char** com) {
-    if(com == NULL){
-        printf("Null argument passed!");
+   
+    
+    clock_gettime(CLOCK_MONOTONIC, &start_time_of_exec);
+
+    int status = fork();
+    if (status < 0) {
+        printf("Process terminated abnormally!");
+        return 0;
+    } else if (status == 0) {
+       
+        execvp(com[0], com);
+   
+        perror("execvp");
+        exit(1);
+    } else {
+     
+        pid = wait(&status);
+ 
+        clock_gettime(CLOCK_MONOTONIC, &end_time_of_exec);
+       
+        return 1;
+    }
+}
+
+static void syscall_handler(int signum) {
+    if (signum == SIGINT) {
+        printf("\n");
+        printf("Ctrl-C pressed....\n");
+        printf("----------------------------------------------------------------------------------------------\n");
+        printf("Program History:\n");
+        printf("\n");
+        history();
+        printf("\n");
+        printf("----------------------------------------------------------------------------------------------\n");
+        printf("Program terminated!\n");
         exit(0);
     }
-    clock_gettime(CLOCK_MONOTONIC,&start_time_of_exec);
-    int status = fork();
-    if(status < 0) {
-        printf("Something bad happened\n");
-        return 0;
-    } else if(status == 0) {
-        getpid();
-        if(strcmp(com[0],"echo")==0){
-            execv(com[0], com);
-        }
-        else{
-            printf("8\n");
-            execv(com[0],com);
-        }
-    }
-    else{
-        int ret;
-        pid = wait(&ret);
-        clock_gettime(CLOCK_MONOTONIC,&end_time_of_exec);
-    }
-    return 1;
 }
 
 void shell_loop() {
-    //Declaring status variable
+    struct sigaction sig;
+    memset(&sig, 0, sizeof(sig));
+    sig.sa_handler = syscall_handler;
+    sigaction(SIGINT, &sig, NULL);
+
+    char* com[MAX_INPUT_LENGTH];
     int status = 1;
-    // Creating / Opening the history file
-    f1 = fopen("history.txt","w+");
-    //Checking if the file opened properly or not
-    if(f1 == NULL){
-        printf("Error in opening file!");
+    f1 = fopen("history.txt", "w+");
+    if (f1 == NULL) {
+        printf("Error in opening file!\n");
         exit(1);
     }
-    //Initialising the variable to store the duration each command takes to execute
     int duration;
-    //Initialising the variable to store the array returned from the split function
-    char** com;
-    //Initialising the variable for reading each character from the file
-    int c;
-    //Initialising the variable to store the details of each command in the history file
-    char line[250] = "";
-    if(line == NULL){
-        printf("Error in allocating memory to line via malloc!");
-        exit(2);
-    }
+    
     char s1[50];
-    //Repeatedly asking for commands until the program gets the Ctrl-C command
     do {
         printf("iiitd@system:~$ ");
-        char *command = malloc(MAX_INPUT_LENGTH);  // Allocate memory for user input
+        char* command = malloc(MAX_INPUT_LENGTH);
         if (command == NULL) {
-            perror("Memory allocation error");
+            printf("Memory allocation error\n");
             exit(3);
         }
         if (fgets(command, MAX_INPUT_LENGTH, stdin) == NULL) {
-            free(command);  // Free the memory before exiting in case of an error
-            perror("Error reading input");
+            free(command);
+            printf("Error reading input\n");
             exit(4);
         }
-        //Calling the get_time function to record the start time of the execution of the entered command in the timeofexec variable.
-        com = split(command);
-        if(strcmp(com[0],"history") == 0){
-            while((c = fgetc(f1)) != EOF){
-                putchar(c);
-            }
+        if (command == NULL) {
+            continue;
         }
-        else{
+        command[strcspn(command, "\n")] = '\0';
+        split(command, com);
+        if (strcmp(com[0], "history") == 0) {
+            history();
+        } else {
+           
+
+            
             status = create_process_and_run(com);
-            //Calculating the time duration (in millisecs) required to execute the command.
-            duration = ((end_time_of_exec.tv_sec - start_time_of_exec.tv_sec)*1000) + ((end_time_of_exec.tv_nsec - start_time_of_exec.tv_nsec)/1000000);
-            strcat(line,"Command: ");
-            strcat(line,command);
-            strcat(line,"\tPID: ");
-            sprintf(s1,"%d",pid);
-            strcat(line,s1);
-            strcat(line,"\tExecution Start Time: ");
-            strcat(line,timeofexec);
-            strcat(line,"\tExecution Duration: ");
-            sprintf(s1,"%d",duration);
-            strcat(line,s1);
-            fputs(line,f1);
+            printf("sdcs");
+            printf("status %d\n",status);
+
+            setenv("TZ", "Asia/Kolkata", 1);
+            tzset();
+            time_t current_time;
+            time(&current_time);
+            struct tm *timeinfo = localtime(&current_time);
+            strftime(timeofexec, sizeof(timeofexec), "%d-%m-%Y %H:%M:%S", timeinfo);
+
+           
+            duration = ((end_time_of_exec.tv_sec - start_time_of_exec.tv_sec) * 1000) + ((end_time_of_exec.tv_nsec - start_time_of_exec.tv_nsec) / 1000000);
+            strcat(line, "Command: ");
+            strcat(line, command);
+            strcat(line, "\tPID: ");
+            sprintf(s1, "%d", pid);
+            strcat(line, s1);
+            strcat(line, "\tExecution Start Time: ");
+            strcat(line, timeofexec);
+            strcat(line, "\tExecution Duration: ");
+            sprintf(s1, "%d", duration);
+             
+            strcat(line, s1);
+            strcat(line, "\tmilliseconds ");
+            strcat(line, "\n"); 
+           
+           
+
+           
         }
-    } while(!status);
+    } while (status);
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const* argv[]) {
     shell_loop();
+    
     return 0;
 }
