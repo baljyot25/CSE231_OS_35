bc2b37efd4dfdec5a2ae969698bb8007823cfb92
Debjit-Banerji <101322330+Debjit-Banerji@users.noreply.github.com>
2023-10-10 21:15:36 +0530

    Update and rename A3.c to shell.c

--- OS_assignement_3/shell.c
+++ OS_assignement_3/shell.c
@@ -9,6 +9,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <signal.h>
+#include "scheduler.c"
 
 #define MAX_INPUT_LENGTH 1024
 #define MAX_LINE_LENGTH 100000
@@ -48,7 +49,6 @@ typedef struct queue{
     Node* end;
 }Queue;
 
-
 Process* com_arr;
 
 char* normal_com[MAX_INPUT_LENGTH];
@@ -56,70 +56,7 @@ int count = 0;
 
 Queue* q;
 
-void create_queue(){
-    q = (Queue*)malloc(sizeof(Queue));
-     printf("inside create_process  %d\n", q==NULL);
-    if(!q){
-        printf("Memory allocation error for queue!");
-        exit(7);
-    }
-    q->front = q->end = NULL;
-}
-
-void enqueue(Process* p){
-    Node* newnode = (Node*)malloc(sizeof(Node));
-    if(!newnode){
-        printf("Memmory allocation error for new node!");
-        exit(8);
-    }
-    newnode->process_data = p;
-    newnode->next = NULL;
-    if(!q->end){
-        q->front = q->end = newnode;
-        return;
-    }
-    q->end->next = newnode;
-    q->end = newnode;
-}
-
-Process* dequeue(){
-    if(!q->front){
-        printf("Scheduler Table is empty!");
-        return NULL;
-    }
-    Node* temp = q->front;
-    Process* p = temp->process_data;
-    q->front = temp->next;
-    if(!q->front) q->end = NULL;
-    free(temp);
-    return p;
-}
-
-int isEmpty(){
-    printf("isempty start");
-    if(q->front == NULL && q->end == NULL){
-        printf("isempty end");
-        return 1;
-    }
-    else{
-        printf("isempty end");
-        return 0;
-    }
-}
-
-static void syscall_handler(int signum);
-
-void set_alarm(unsigned int ms) {
-    signal(SIGALRM, syscall_handler); // Register the signal handler
-    unsigned int s = ms / 1000; // Convert milliseconds to seconds
-    unsigned int us = (ms % 1000) * 1000; // Convert remaining milliseconds to microseconds
-    struct itimerval timer;
-    timer.it_value.tv_sec = s;
-    timer.it_value.tv_usec = us;
-    timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 0;
-    setitimer(ITIMER_REAL, &timer, NULL); // Set the timer
-}
+//static void syscall_handler(int signum);
 
 //Function to parse the command and transform it into a 2d array for easier use 
 int split(char* command) {
@@ -147,10 +84,9 @@ int split(char* command) {
             com_arr->com[i] = s1;
             i++;
         }
-
         com_arr->com[i] = NULL;
         enqueue(com_arr);
-        count = 1;
+        count += 1;
         if(clock_gettime(CLOCK_MONOTONIC, &com_arr->start_time) == -1){
             printf("Error executing clock_gettime!");
             exit(1);
@@ -168,43 +104,29 @@ int split(char* command) {
     return 2;   
 }
 
-//Function to calculate start time of execution of a command
-void get_time() {
-    //Setting the time zone
-    setenv("TZ", "Asia/Kolkata", 1);
-    tzset();
-    time_t current_time;
-    //Getting the current time 
-    time(&current_time);
-    //Converting the time to the local time
-    struct tm *timeinfo = localtime(&current_time);
-    //Converting the format of the time we got to a readable and understandable format
-    strftime(timeofexec, sizeof(timeofexec), "%d-%m-%Y %H:%M:%S", timeinfo);
-}
+// //Function to calculate start time of execution of a command
+// void get_time() {
+//     //Setting the time zone
+//     setenv("TZ", "Asia/Kolkata", 1);
+//     tzset();
+//     time_t current_time;
+//     //Getting the current time 
+//     time(&current_time);
+//     //Converting the time to the local time
+//     struct tm *timeinfo = localtime(&current_time);
+//     //Converting the format of the time we got to a readable and understandable format
+//     strftime(timeofexec, sizeof(timeofexec), "%d-%m-%Y %H:%M:%S", timeinfo);
+// }
 
-//Function to get the history (all commands that have been issued so far)
+//Function to get the history (all commands that have been submitted into the scheduler)
 void history() {
-    // //Puts the entire content that has been stored in line into the history file
-    // int r = fputs(line, f1);
-    // if(r == EOF){
-    //     printf("Fputs error!");
-    //     exit(1);
-    // }
-
-    // int c;
-    // //Sets the file pointer to the beginning of the history file
-    // rewind(f1); 
-    // //Prints all the content of the history file
-    // while ((c = fgetc(f1)) != EOF) {
-    //     putchar(c);
-    // }
-    // //Empties the line variable and readies it for more commands to be added for storing in the history
-    // memset(line, '\0', sizeof(line));
-
-
-    // for(int i = 0;i<rows;i++){
-    //     printf("%d. Command Name: %s\t PID: %d\t Execution Time: %f\t Waiting Time: %f\n",(i+1),com_arr->com_name,(int)com_arr->pid,com_arr[i]->exec_time,com_arr[i]->waiting_time);
-    // }
+    int c;
+    //Sets the file pointer to the beginning of the history file
+    rewind(f1); 
+    //Prints all the content of the history file
+    while ((c = fgetc(f1)) != EOF) {
+        putchar(c);
+    }
 }
 
 //Function to run the command entered
@@ -226,95 +148,6 @@ int create_process_and_run1(){
     }
 }
 
-int create_process_and_run2(Process* p) {
-     int status=p->pid = fork(); //Creates a child process
-   
-    pid = p->pid;
-    if (status < 0) { //Handles the case when the child process terminates abruptly
-        printf("Process terminated abnormally!");
-        return 0;
-    } else if (status == 0) { //Child process
-        if(clock_gettime(CLOCK_MONOTONIC, &p->start_time) == -1){
-            printf("Error executing clock_gettime!");
-            exit(1);
-        }
-        // Start the timer
-        set_alarm(tslice);
-        //Executes the command by using the inbuilt execvp function
-        if (execvp(p->com[0], p->com) == -1) {
-            fprintf(stderr, "Error executing command.\n");
-            exit(1);
-        }
-    }
-    else{ //Parent process
-        wait(NULL);
-        p->f1 = 2;
-        //Stores the end time of the execution of the child process
-        if(clock_gettime(CLOCK_MONOTONIC, &p->end_time) == -1){
-            printf("Error executing clock_gettime!");
-            exit(1);
-        }
-        p->exec_time+=(double)((p->end_time.tv_sec - p->start_time.tv_sec) * 1000.0) + ((p->end_time.tv_nsec - p->start_time.tv_nsec) / 1000000.0);
-        return 1;
-    }
-}
-
-void run_existing_process(Process* p){
-    //End waiting time
-    if(clock_gettime(CLOCK_MONOTONIC, &p->end_time) == -1){
-        printf("Error executing clock_gettime!");
-        exit(1);
-    }
-    //Add to the waiting time of the process
-    p->waiting_time += (double)((p->end_time.tv_sec - p->start_time.tv_sec) * 1000.0) + ((p->end_time.tv_nsec - p->start_time.tv_nsec) / 1000000.0);
-
-    // //Start the timer for the process to start execution again
-    // if(clock_gettime(CLOCK_MONOTONIC, &p->start_time) == -1){
-    //     printf("Error executing clock_gettime!");
-    //     exit(1);
-    // }
-    //Start the timer for execution time.
-    kill(p->pid,SIGCONT);
-    //Wait for the tslice and stop the process after the tslice is over.
-    set_alarm(tslice);
-    // //Stop the timer for the execution time.
-    // if(clock_gettime(CLOCK_MONOTONIC, &p->end_time) == -1){
-    //     printf("Error executing clock_gettime!");
-    //     exit(1);
-    // }
-    if(p->f1 != 2){
-        //Add to the execution time of the process.
-        p->exec_time+=tslice;
-        //Enqueuing the process again to finsih its execution later.
-        enqueue(p);
-    }
-    //Starting the timer for wait time.
-    if(clock_gettime(CLOCK_MONOTONIC, &p->start_time) == -1){
-        printf("Error executing clock_gettime!");
-        exit(1);
-    }
-}
-
-void round_robin(){
-    while(!isEmpty()){
-        Process* p = NULL;
-        int limit = (ncpus>count) ? ncpus : count;
-        for(int i = 0;i<limit;i++){
-            p = dequeue();
-            if(p->f1 == 0){
-                create_process_and_run2(p);
-                if(clock_gettime(CLOCK_MONOTONIC, &p->end_time) == -1){
-                    printf("Error executing clock_gettime!");
-                    exit(1);
-                }
-            }
-            else if(p->f1 == 1){
-                run_existing_process(p);
-            }
-        }
-    }
-}
-
 //Handles the default and custom signals.
 static void syscall_handler(int signum) {
     if (signum == SIGINT) {
@@ -323,7 +156,7 @@ static void syscall_handler(int signum) {
         printf("----------------------------------------------------------------------------------------------\n");
         printf("Program History:\n");
         printf("\n");
-        // history();
+        history();
         printf("\n");
         printf("----------------------------------------------------------------------------------------------\n");
         printf("Program terminated!\n");
@@ -351,12 +184,12 @@ void shell_loop() {
 
     // parent_pid = getppid();
     //Opens the history.txt file and checks if the file has been opened correctly or not
-    // f1 = fopen("history.txt", "w+");
-    // if (f1 == NULL) {
-    //     printf("Error in opening file!\n");
-    //     exit(1);
-    // }
-    // double duration;
+    f1 = fopen("history.txt", "w+");
+    if (f1 == NULL) {
+        printf("Error in opening file!\n");
+        exit(1);
+    }
+    double duration;
 
 
     //Initialising a variable to read ncpus and tslice from the stdin
