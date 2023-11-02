cfb10b3168d1ff2b031d7690aa42d0c052260e23
baljyot25 <132061122+baljyot25@users.noreply.github.com>
2023-11-02 16:07:20 +0530

    Update loader.c

--- OS_Assignment_4/loader/loader.c
+++ OS_Assignment_4/loader/loader.c
@@ -2,7 +2,6 @@
 
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
-Elf32_Phdr *phdr1;
 int fd;
 int j = 0;
 size_t offset_vmem;
@@ -22,6 +21,7 @@ void loader_cleanup() {
 
 void sigsegv_handler(int signum, siginfo_t *info, void *context){
   if(signum == SIGSEGV){
+    j++;
     printf("sigsegv invoked!\n");
     //positioning the file pointer to the section from where the program header table starts
     printf("info: %d\n",(int)info->si_addr);
@@ -44,22 +44,55 @@ void sigsegv_handler(int signum, siginfo_t *info, void *context){
         int n_page=phdr->p_memsz%4096==0? phdr->p_memsz/4096 : (phdr->p_memsz/4096)+1;
         // virtual_mem = mmap((void*)(phdr->p_vaddr),n_page*4096, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_FIXED, fd, phdr->p_offset);
         printf("n_page: %d\n",n_page);
-        if(phdr1 != phdr) {
-          j = 0;
-          phdr1 = phdr;
-        }
-        virtual_mem = mmap((void*)(phdr->p_vaddr+j*4096),4096, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_FIXED, fd, phdr->p_offset);
-        printf("page %d allocated for fault address: %d\n",(j+1),(int)fault_addr);
-        j++;
+        int i=((int)(fault_addr - phdr->p_vaddr))/4096;
+        virtual_mem = mmap((void*)(phdr->p_vaddr+i*4096),4096, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_FIXED, fd, phdr->p_offset);
+        
+        
+        printf("1\n");
         //throws error virtual_mem is not allocated properly
         if(virtual_mem == MAP_FAILED){
           printf("Error in defining vitual_mem!\n");
           loader_cleanup();
           exit(4);
         }
+        printf("here\n");
+        if(fault_addr == (void*)ehdr->e_entry){
+          offset_vmem = ehdr->e_entry - phdr->p_vaddr;
+          printf("offset: %d\n",offset_vmem);
+        }
+        printf("5\n");
         break;
-      } 
+      }  
+      
     }
+    // lseek(fd,(ehdr->e_phoff + i*ehdr->e_phentsize),SEEK_SET);
+    // //initialising phdr
+    // phdr = (Elf32_Phdr*)malloc(sizeof(Elf32_Phdr));
+    // //reading the segment and making the phdr point to the segment
+    // if(read(fd,phdr,sizeof(Elf32_Phdr)) != sizeof(Elf32_Phdr)){
+    //   printf("The program header couldn't be read!");
+    //   exit(3);
+    // }
+    // int num = (phdr->p_memsz)/4096;
+    // printf("Internal frag: %d\n",4096 - phdr->p_memsz);
+    // virtual_mem = mmap((void*)phdr->p_vaddr,(num*4096 == phdr->p_memsz) ? num*4096 : (num+1)*4096, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_FIXED, fd, phdr->p_offset);
+    // //throws error virtual_mem is not allocated properly
+    // if(virtual_mem == MAP_FAILED){
+    //   printf("Error in defining vitual_mem!\n");
+    //   loader_cleanup();
+    //   exit(4);
+    // }
+    // printf("e_entry: %d\n",ehdr->e_entry);
+    // printf("p_vaddr: %d\n",phdr->p_vaddr);
+    // printf("upper limit: %d\n",phdr->p_vaddr + phdr->p_memsz);
+    // if((ehdr->e_entry >= phdr->p_vaddr) && (ehdr->e_entry < (phdr->p_vaddr + phdr->p_memsz))){
+    //   printf("4\n");
+    //   offset_vmem = ehdr->e_entry - phdr->p_vaddr;
+    //   printf("offset: %d\n",offset_vmem);
+    //   // break;
+    // }
+    // i++;
+    // printf("end\n");
   }
 }
 
@@ -87,13 +120,15 @@ void load_and_run_elf(char** exe) {
     }
     // initialising phdr
     phdr = (Elf32_Phdr*)malloc(sizeof(Elf32_Phdr));
-    phdr1 = (Elf32_Phdr*)malloc(sizeof(Elf32_Phdr));
+    // int (*_start)() = (int (*)())((char*)ehdr->e_entry);
+    // _start();
   }
   else{
     printf("Error in opening the elf file!\n");
   }
-  //Typecasting the e_entry address to the start function pointer to facilitate the function call in the subsequent lines
+  printf("out of loop\n");
   int (*_start)() = (int (*)())((char*)ehdr->e_entry);
+  //Typecasting the e_entry address to the start function pointer to facilitate the function call in the subsequent lines
   int result = _start();
   printf("User _start return value = %d\n",result);
 }
